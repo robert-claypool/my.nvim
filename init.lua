eb74bfc6c3e3cb17d1273cc69e9cb0a7e87379ae
Robert Claypool <robert-claypool@outlook.com>
2025-05-27 23:47:41 -0500

    lol, now it works

--- init.lua
+++ init.lua
@@ -212,6 +212,9 @@ require('lazy').setup({
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
+
+      -- Additional lua configuration, makes nvim stuff amazing!
+      'folke/neodev.nvim',
     },
   },
 
